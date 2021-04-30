35590014293ee42922a615fa8aa8563dcdbf0766
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-04-30 11:24:06 -0400

    Revert "Revert "Update script.js""
    This reverts commit 2964c3a743cd628235aa5005cdb65ceebbd7aea0.

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-import EasyJSVersion from "https://gavinmorrow.github.io/EasyJS/";
-const EasyJS = new EasyJSVersion(1);
+import EasyJSVersion from "https://gavinmorrow.github.io/EasyJS/main.js";
+const EasyJS = await EasyJSVersion(1);
 const colorScheme = EasyJS.ui.colorScheme;
