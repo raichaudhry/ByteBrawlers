503e29c5ddc23994ec94b913f18e15d6f5149843
gavinmorrow <72874398+gavinmorrow@users.noreply.github.com>
2021-05-03 18:20:17 -0400

    Stuff

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 import EasyJSVersion from "https://gavinmorrow.github.io/EasyJS/main.js";
 const EasyJS = await EasyJSVersion(1);
 const colorScheme = EasyJS.ui.colorScheme;
+
+window.login = async () => {
+	alert("Loging in…");
+}
