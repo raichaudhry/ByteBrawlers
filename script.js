59299c0f82f93ed77795bdb1b2d59eb51fda10ed
gavinmorrow <gavinfmorrow@gmail.com>
2021-05-10 13:57:35 -0400

    Stuff

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import colorScheme from "https://gavinmorrow.github.io/EasyJS/1/ui/colorScheme/index.js";
+import Popup from "https://gavinmorrow.github.io/EasyJS/1/ui/popup/index.js";
 colorScheme.setColors({
 	bg: "#ffffff",
 	navBg: "#fefffe",
@@ -30,7 +31,7 @@ window.login = async () => {
 	const pass = document.getElementById("pass");
 	const pop = document.getElementById("error");
 	const wrapper = document.getElementById("error").parentElement;
-	const passHashed = await fetch(`/users/${username.value}/pass.txt`).then(r => r.text());
+	const passHashed = await fetch(`/users/${username.value}/pass.txt`).then(r => r.text()).catch(e => `${e}`);
 	const hash = string => {
 		let output = "";
 		for (let letter of string) {
@@ -40,24 +41,29 @@ window.login = async () => {
 		}
 		return output;
 	};
-	const popup = up => {
-		pop.innerHTML = up;
-		wrapper.style.zIndex = "1000";
-		wrapper.style.opacity = "1";
-		setTimeout(() => {
-			// wrapper.style.opacity = "0";
-			// setTimeout(() => {
-			// 	wrapper.style.zIndex = "-1000";
-			// }, 1000);
-		}, 5000);
-	}
-	if (passHashed == hash(pass.value)) popup("Logged in!");
-	else popup("Nope!");
+	// const popup = up => {
+	// 	pop.innerHTML = up;
+	// 	wrapper.style.zIndex = "1000";
+	// 	wrapper.style.opacity = "1";
+	// 	setTimeout(() => {
+	// 		// wrapper.style.opacity = "0";
+	// 		// setTimeout(() => {
+	// 		// 	wrapper.style.zIndex = "-1000";
+	// 		// }, 1000);
+	// 	}, 5000);
+	// }
+	if (passHashed == hash(pass.value)) new Popup("Logged in!").show();
+	else new Popup("Nope!").show();
 }
 
 const passToggle = document.getElementById("pass-toggle");
 passToggle.addEventListener("change", e => {
-	if (passToggle.checked) document.getElementById("pass").type = "text";
-	else document.getElementById("pass").type = "password";
+	if (passToggle.checked) {
+		document.getElementById("pass").type = "text";
+		document.getElementById("pass-toggle-label").innerHTML = "Hide Password";
+	} else {
+		document.getElementById("pass").type = "password";
+		document.getElementById("pass-toggle-label").innerHTML = "Show Password";
+	}
 });
 passToggle.style.setProperty("--size", getComputedStyle(document.getElementById("pass-toggle-label")).fontSize);
