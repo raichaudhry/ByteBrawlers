3b0e6d1411ce5d21f30e1a7bf90dc04e9c9da7b0
gavinmorrow <gavinfmorrow@gmail.com>
2021-05-17 08:36:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ colorScheme.setColors({
 	link: "#adddad",
 });
 cookies.cookieConsent();
-console.log(cookies.interface);
+console.log(cookies.Cookie);
 
 window.login = async () => {
 	const username = document.getElementById("username");
