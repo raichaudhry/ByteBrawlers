f2e5b0ae6481c9e29aabe13148916c6731b75c85
gavinmorrow <gavinfmorrow@gmail.com>
2021-05-27 17:18:55 -0400

    PATCH launcher widget

--- launcher/widgets/colorScheme.js
+++ launcher/widgets/colorScheme.js
@@ -1,5 +1,7 @@
 import colorScheme from "https://gavinmorrow.github.io/EasyJS/1/ui/colorScheme/index.js";
+import Cookie from "https://gavinmorrow.github.io/EasyJS/1/cookies/cookie/index.js";
 
+const csc = new Cookie("cs", "auto");
 colorScheme.autoChange = false;
 
 const widget = document.getElementById("cs-widget");
@@ -7,18 +9,17 @@ const inputs = widget.querySelectorAll("input");
 
 const updateCS = () => {
 	for (const input of inputs) {
-		if (input.checked) {
-			switch (input.value) {
-				case "light":
-					colorScheme.set(true);
-					break;
-				case "dark":
-					colorScheme.set(false);
-					break;
-				default:
-					colorScheme.reset();
-			}
-		}
+		if (input.checked) csc.value = input.value;
+	}
+	switch (csc.value) {
+		case "light":
+			colorScheme.set(true);
+			break;
+		case "dark":
+			colorScheme.set(false);
+			break;
+		default:
+			colorScheme.reset();
 	}
 }
 updateCS();
