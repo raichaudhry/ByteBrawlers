24a61dd841683cadd97911b0bbf249853c96a331
Rai Chaudhry <72818839+raichaudhry@users.noreply.github.com>
2021-06-06 11:01:01 -0400

    Stuff

--- signup/script.js
+++ signup/script.js
@@ -8,8 +8,17 @@ window.submit = function () {
     let name = `${first} ${last}`;
     let username = document.getElementById("username").value;
     let email = document.getElementById("email").value;
+    let pass = document.getElementById("pass").value;
+    let confirmpass = document.getElementById("confirmpass").value;
+    if (pass !== confirmpass) {  
+    alert("Whoops! It looks like you entered different passwords for the password and confirm password fields. Please click 'OK' and try again.")
+    location.reload;
+    } else {
+        console.log("pass test #1, passed.")
+    }
     console.log('Welcome,' + username + '! Confirming we can reach you at ' + email)
     console.log(`Welcome to ByteBrawlers, ${name}!`);
-    fetch('index.php?${email}+${name}+${username}')
+    fetch('index.php?${email}+${name}+${username}+$(first)+${last}+${pass}+${confirmpass}')
+    location.href="../"
 };
 startup();
