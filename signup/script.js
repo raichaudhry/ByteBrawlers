32bffd3b8f051e37497a56ffe2164625ba979960
Rai Chaudhry <72818839+raichaudhry@users.noreply.github.com>
2021-06-06 10:20:17 -0400

    Signup Stuff

--- signup/script.js
+++ signup/script.js
@@ -1,12 +1,15 @@
 function startup () {
     // document.getElementById("challenge2").style.display = "none";
 }
-function submit () {
-    // document.getElementById("challenge2").style.display = "block";
+window.submit = function () {
     document.getElementById("challenge1").style.display = "none";
     let first = document.getElementById("first").value;
     let last = document.getElementById("last").value;
     let name = `${first} ${last}`;
-    console.log(`Welcome, ${name}!`);
-}
+    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+    console.log('Welcome,' + username + '! Confirming we can reach you at ' + email)
+    console.log(`Welcome to ByteBrawlers, ${name}!`);
+    fetch('index.php?${email}+${name}+${username}')
+};
 startup();
