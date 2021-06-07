14ccfc6d511318ab334db5283918788e476eff14
Rai Chaudhry <72818839+raichaudhry@users.noreply.github.com>
2021-06-07 15:09:34 -0400

    a

--- signup/script.js
+++ signup/script.js
@@ -14,13 +14,11 @@ window.submit = function () {
     let confirmpass = document.getElementById("confirmpass").value;
     if (pass !== confirmpass) {  
     alert("Whoops! It looks like you entered different passwords for the password and confirm password fields. Please click 'OK' and try again.");
-    break;
 } else {
 	// const popupOptions = [2500, true];
 	// const popup2 = new Popup("Please enter the correct username and password.", ...popupOptions, "error");    }
     if (pass.length < 5) {
         alert("Uh-oh! It looks like your password is shorter than 5 characters. Please try again.");
-        break;
     } else {
         console.log("pass test #2, passed.");
     }
