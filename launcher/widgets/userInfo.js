4aeb9c898ea680dffda4229cfd23621d8792b889
gavinmorrow <gavinfmorrow@gmail.com>
2021-06-10 15:21:19 -0400

    Fix URLs

--- launcher/widgets/userInfo.js
+++ launcher/widgets/userInfo.js
@@ -1,7 +1,7 @@
 import parseInfo from "/js/parseInfo.js";
 import Cookie from "https://gavinmorrow.github.io/EasyJS/1/cookies/cookie/index.js";
 const username = Cookie.get("username").value;
-const data = await fetch(`http://bb-data.mateopaula.com/users/${username}/info.txt`).then(data => data.text());
+const data = await fetch(`http://gavinmorrow.com/bb/data/users/${username}/info.txt`).then(data => data.text());
 const parsed = parseInfo(data);
 for (const name in parsed) {
 	const data = parsed[name];
