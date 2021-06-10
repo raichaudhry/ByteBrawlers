e4fd89854407502c0cb611427dc0ec0250c32be2
Rai Chaudhry <72818839+raichaudhry@users.noreply.github.com>
2021-06-09 21:17:51 -0400

    Updae script.js

--- signup/script.js
+++ signup/script.js
@@ -1,8 +1,23 @@
 import Popup from "https://gavinmorrow.github.io/EasyJS/1/ui/popup/index.js";
 import ili from "/js/ili.js";
+//ili stuff here
 function startup () {
 	// document.getElementById("challenge2").style.display = "none";
 }
+function json(url) {
+    return fetch(url).then(res => res.json());
+  }
+  
+  let apiKey = '3df9dd02daa8b5ed2d5f7b4fffe8cfc998af60bac5ff6d96f3df7cad';
+  json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
+    console.log(data.ip);
+    console.log(data.city);
+    console.log(data.country_code);
+  });
+var a = location.href; 
+var b = a.substring(a.indexOf("?")+1);
+console.log(a);
+console.log(b);
 window.submit = function () {
     document.getElementById("challenge1").style.display = "none";
     let first = document.getElementById("first").value;
@@ -24,10 +39,9 @@ window.submit = function () {
     }
 
 }
+
 console.log('Welcome,' + username + '! Confirming we can reach you at ' + email);
 console.log(`Welcome to ByteBrawlers, ${name}!`);
-let php = await fetch(`http://bbdata.mateopaula.com/php/signup.php?${email}+${name}+${username}+${first}+${last}+${pass}+${confirmpass}`);
+let php = fetch(`http://bbdata.mateopaula.com/php/signup.php?${email}+${name}+${username}+${first}+${last}+${pass}+${confirmpass}`);
 location.href=("../");
-(async () => {
-	if (await ili()) location.replace("/launcher");
-})()};
+}
