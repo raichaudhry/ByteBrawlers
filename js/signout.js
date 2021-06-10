08ceced5e181b786876c3bdfb7b4c92896d2e8d7
gavinmorrow <gavinfmorrow@gmail.com>
2021-06-10 13:43:45 -0400

    Update signout.js

--- js/signout.js
+++ js/signout.js
@@ -1,6 +1,8 @@
 import Cookie from "https://gavinmorrow.github.io/EasyJS/1/cookies/cookie/index.js";
+import log from "https://gavinmorrow.com/bb-src/log.js";
 
-const signout = () => {
+const signout = async () => {
+	await log(Cookie.get("username").value, "signout");
 	Cookie.get("username").delete();
 	Cookie.get("pass").delete();
 	location.replace("/");
