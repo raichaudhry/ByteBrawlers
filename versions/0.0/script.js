1c4ce6e7586d5f191d9aca455803ca18159f58f0
gavinmorrow <gavinfmorrow@gmail.com>
2021-06-14 17:40:19 -0400

    Add friending functionality (will need patches and upgrades).
    Co-authored-by:

--- versions/0.0/script.js
+++ versions/0.0/script.js
@@ -5,13 +5,22 @@ const srcURL = "https://gavinmorrow.com/bb/src";
 const databaseURL = "https://gavinmorrow.com/bb/data";
 const friendsElem = document.getElementById("friends");
 const friendsList = document.getElementById("friendsList");
-const errorFuncs = {};
-const username = Cookie.get("username").value;
-errorFuncs.friendFetch = e => {
-	friendsElem.innerHTML = `There was an error. Refresh the page and try again.<br/><br/>Error:<br/>${e}`;
-	console.error(e);
-	return e;
+const getErrorFunc = func => {
+	return e => {
+		func(e);
+		console.error(e);
+		return e;
+	}
 }
+const errorFuncs = {
+	friendFetch: getErrorFunc(e => friendsElem.innerHTML = `There was an error. Refresh the page and try again.<br/><br/>Error:<br/>${e}`),
+	addFriend: getErrorFunc(async e => {
+		const popup = await new Popup(`There was an error. Refresh the page and try again. <br/><br/>${e}`, 5000, false, true);
+		await popup.show();
+		await popup.hide();
+	}),
+};
+const username = Cookie.get("username").value;
 // Fetch friends
 (async () => {
 	const file = await fetch(`${srcURL}/file.php?username=${username}&path=/0/&name=friends.txt`).then(r => r.text()).catch(errorFuncs.friendFetch);
@@ -41,6 +50,8 @@ document.getElementById("add-friend").addEventListener("click", async () => {
 	const popup = await new Popup(`Friend's Username: <input id="friend-username" type="text"><br/><br/><button id="submit-friend">Find!</button>`, 0, false, "friend-username-wrapper", true);
 	await popup.show();
 	document.getElementById("submit-friend").addEventListener("click", async () => {
-		const addFriend = await fetch(`${srcURL}/addFriend.php?username=${username}&fusername=${document.getElementById("friend-username").value}`);
+		const addFriend = await fetch(`${srcURL}/addFriend.php?username=${username}&fusername=${document.getElementById("friend-username").value}`).then(r => r.text()).catch(errorFuncs.addFriend);
+		console.log(addFriend);
+		await popup.hide();
 	});
 });
