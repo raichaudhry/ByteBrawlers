b96d78649d83aa3f32a42c6b279d09cf90350aab
gavinmorrow <gavinfmorrow@gmail.com>
2021-06-24 12:34:20 -0400

    Save Backup

--- versions/0.0/script.js
+++ versions/0.0/script.js
@@ -5,6 +5,7 @@ const srcURL = "https://gavinmorrow.com/bb/src";
 const databaseURL = "https://gavinmorrow.com/bb/data";
 const friendsElem = document.getElementById("friends");
 const friendsList = document.getElementById("friendsList");
+const friendsListInitInnerHTML = friendsList.innerHTML;
 const getErrorFunc = func => {
 	return (e, ...args) => {
 		func(e, ...args);
@@ -27,8 +28,8 @@ const errorFuncs = {
 };
 const username = Cookie.get("username").value;
 // Fetch friends
-(async () => {
-	const file = await fetch(`${srcURL}/file.php?username=${username}&path=/0/&name=friends.txt`).then(r => r.text()).catch(errorFuncs.friendFetch);
+const fetchFriends = async () => {
+	const file = await fetch(`${srcURL}/file.php?username=${username}&path=/&name=friends.txt`).then(r => r.text()).catch(errorFuncs.friendFetch);
 	switch (file) {
 		case "0":
 			// Friends text file exists.
@@ -40,9 +41,9 @@ const username = Cookie.get("username").value;
 			errorFuncs.friendFetch();
 			break;
 	}
-	await fetch(`${srcURL}/file.php?username=${username}&path=/&name=friends.txt`).catch(errorFuncs.friendFetch);
 	const friendsString = await fetch(`${databaseURL}/users/${username}/friends.txt`).then(r => r.text());
 	const friends = friendsString.split("\n");
+	friendsList.innerHTML = friendsListInitInnerHTML;
 	for (const friendLine of friends) {
 		const [friend, flag] = friendLine.split(" ");
 		if (friend.length < 1) continue;
@@ -52,19 +53,33 @@ const username = Cookie.get("username").value;
 		`.trim();
 		friendsList.appendChild(item);
 	}
-})();
-const popup = await new Popup(`Friend's Username: <input id="friend-username" type="text"><br/><br/><button id="submit-friend">Find!</button>`, 0, false, "friend-username-wrapper", true);
-document.getElementById("add-friend").addEventListener("click", async () => {
-	await popup.show();
-	document.getElementById("submit-friend").addEventListener("click", async () => {
-		const addFriend = await fetch(`${srcURL}/addFriend.php?username=${username}&fusername=${document.getElementById("friend-username").value || "parzival"}`).then(r => r.text()).catch(errorFuncs.addFriend);
-		switch (addFriend) {
-			case "0":
-				errorFuncs.addFriend(new Error("The username does not exist."), document.getElementById("friend-username").value);
-				break;
-			default:
-				break;
-		}
-		await popup.hide();
-	});
-});
+};
+fetchFriends();
+const popup = await new Popup(`
+Friend's Username: <form action='javascript:addFriend()'>
+	<input id="friend-username" type="text" required><br/><br/><input type='submit' id="submit-friend" value='Find!'>
+</form>
+`, 0, false, "friend-username-wrapper", true);
+const addFriend = async e => {
+	if (e.target == document.getElementById("add-friend")) {
+		await popup.show();
+		document.getElementById("submit-friend").addEventListener("click", async () => {
+			const addFriend = await fetch(`${srcURL}/addFriend.php?username=${username}&fusername=${document.getElementById("friend-username").value || "parzival"}`, {
+				cache: "no-store",
+			}).then(r => r.text()).catch(errorFuncs.addFriend);
+			switch (addFriend) {
+				case "0":
+					errorFuncs.addFriend(new Error("The username does not exist."), document.getElementById("friend-username").value);
+					break;
+				case "1":
+					fetchFriends();
+					break;
+				default:
+					break;
+				}
+			await popup.hide();
+			document.getElementById("friend-username").value = "";
+		});
+	}
+}
+addEventListener("click", addFriend);
