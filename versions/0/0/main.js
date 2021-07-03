e07eabd3acc54e38d7e3f6f31a7366aa60d32f53
gavinmorrow <gavinfmorrow@gmail.com>
2021-07-03 09:28:14 -0400

    Update main.js
    take away loading log, it is not needed

--- versions/0/0/main.js
+++ versions/0/0/main.js
@@ -1,8 +1,6 @@
 import Cookie from "https://gavinmorrow.github.io/EasyJS/2/cookies/cookie/index.js";
 import Popup from "https://gavinmorrow.github.io/EasyJS/2/ui/popup/index.js";
 
-console.log("Game Script Loaded");
-
 const srcURL = "https://gavinmorrow.com/bb/src";
 const databaseURL = "https://gavinmorrow.com/bb/data";
 const friendsElem = document.getElementById("friends");
